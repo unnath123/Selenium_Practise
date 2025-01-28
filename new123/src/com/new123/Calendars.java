c3aa089091d6c47c015f302e6ab97c6b744fc943
unnath123 <unnathpgowda@gmail.com>
2025-01-29 01:41:25 +0530

    calendars

--- new123/src/com/new123/Calendars.java
+++ new123/src/com/new123/Calendars.java
@@ -1,6 +1,7 @@
 package com.new123;
 
 import java.time.Duration;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.openqa.selenium.By;
@@ -20,30 +21,58 @@ public class Calendars {
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10)); 
 		Thread.sleep(2000);
 		driver.findElement(By.className("commonModal__close")).click();
-		Thread.sleep(2000);
+		
 		driver.findElement(By.xpath("//label[@for='departure']")).click();
 		
-		List<WebElement> elements = driver.findElements(By.className("DayPicker-Caption"));
+		String Month = "February";
+		int date = 26;
 		
-		int index=1;
 		
-//		for(WebElement element : elements) {
-//			WebElement MonthDiv = element.findElement(By.tagName("div"));
-//			if(MonthDiv.getText() == "February") {
-//				
-//			}
-//			else {
-//				index++;
-//			}
+		
+		List<WebElement> elements = driver.findElements(By.xpath("//div[@class='DayPicker-Month']"));
+//		System.out.println(elements.get(1).getText());
+//		DateSelect(elements.get(1), date);
+		
+		for(WebElement element : elements) {
+			if((element.getText()).contains(Month)) {
+				System.out.println("hurray");
+				DateSelect(element, date);
+			}
+			
+		}
+		
+		//WebElement ele1 = driver.findElement(By.xpath("//div[@class='DayPicker-Months']/div[1]"));
+
+//		if(ele1.findElement(By.className("DayPicker-Caption")).getText() == Month) {
+//			DateSelect(ele1, date);
+//			System.out.println("first");
 //		}
-		
-		System.out.println(driver.findElement(By.xpath("//div[@class='DayPicker-Month'][2]//div[@class='DayPicker-Body']")).getText());
-		driver.findElement(By.xpath("//div[@class='DayPicker-Month'][2]//div[@class='DayPicker-Body']//p[text()='2']")).click();
+//		else {
+//			System.out.println("second");
+//			WebElement ele2 = driver.findElement(By.xpath("//div[@class='DayPicker-Months']/div[2]"));
+//			//System.out.println(ele2.findElement(By.className("DayPicker-Caption")).getText());
+//			DateSelect(ele2, date);
+//		}
+//		
+				
+	
+//		
+//		System.out.println(driver.findElement(By.xpath("//div[@class='DayPicker-Month'][2]//div[@class='DayPicker-Body']")).getText());
+//		driver.findElement(By.xpath("//div[@class='DayPicker-Month'][2]//div[@class='DayPicker-Body']//p[text()='2']")).click();
 		
 		Thread.sleep(5000);
-		
-		driver.quit();
+		  
+//		driver.quit();
 	}
+	
+	public static void DateSelect(WebElement element, int date) {
+		System.out.println("third");
+//		System.out.println(element.findElement(By.xpath("//div[@class='DayPicker-Day'][contains(@aria-label,'Feb 20 2025')]")).getText());
+		element.findElement(By.xpath("//div[@class='DayPicker-Day']//p[text()='"+ date +"']")).click();
+		
+	}
+	
+	
 
 	
 }
