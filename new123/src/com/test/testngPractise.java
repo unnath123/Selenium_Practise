1a24aedfc4f6f319c9edd0011eb7c2b6373f37a3
unnath123 <unnathpgowda@gmail.com>
2025-02-10 01:59:36 +0530

    testng practise -1

---
+++ new123/src/com/test/testngPractise.java
@@ -0,0 +1,27 @@
+package com.test;
+
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class testngPractise {
+
+	@BeforeClass
+	public void LaunchBrowser() {
+		System.out.println("Browser Launched");
+	}
+
+	@BeforeMethod
+	public void URL() {
+		System.out.println("URL Launched");
+	}
+	
+	@Test
+	public void Testing1() {
+		System.out.println("Testcases - 1");
+	}
+	@Test
+	public void Testing2() {
+		System.out.println("Testcases - 2");
+	}
+}
